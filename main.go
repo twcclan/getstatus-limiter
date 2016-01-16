dc50e5d806fb95d5361e9c28b24f4e4b8981e532
André Bierlein <andre.mt.bierlein@gmail.com>
2016-01-16 21:22:35 +0100

    Initial commit

---
+++ main.go
@@ -0,0 +1,150 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net"
+	"os/exec"
+	"strings"
+	"time"
+
+	"github.com/google/gopacket"
+	"github.com/google/gopacket/pcap"
+	"github.com/hashicorp/golang-lru"
+	"github.com/juju/ratelimit"
+)
+
+const (
+	// how many packets per second clients are allowed to send on average
+	packetsPerSecond = 50
+
+	// the number of packets a single client may send at most per second
+	burst = 1000
+)
+
+var (
+	// this is a lru cache used to hold the token buckets
+	// that are used for rate limiting
+	cache, _ = lru.New(1024)
+
+	// a map that holds all IP addresses that were banned already
+	// used to prevent double-banning an IP
+	// TODO: should probably forget addresses after some time
+	offenders = make(map[string]bool)
+
+	// this map holds all local ipv4 addresses
+	locals = make(map[string]bool)
+)
+
+// store all local ipv4 addresses
+func getLocalAddresses() {
+	i, _ := net.Interfaces()
+
+	for _, iface := range i {
+		addrs, _ := iface.Addrs()
+
+		for _, addr := range addrs {
+			var ip net.IP
+			switch v := addr.(type) {
+			case *net.IPNet:
+				ip = v.IP
+			case *net.IPAddr:
+				ip = v.IP
+			}
+
+			ipv4 := ip.To4()
+			if ipv4 != nil {
+				locals[ipv4.String()] = true
+			}
+		}
+	}
+}
+
+// retrieve a token bucket from the cache, keyed by ip address
+func getRateLimit(key string) *ratelimit.Bucket {
+
+	// get or create the token bucket for this ip address
+	cache.ContainsOrAdd(key, ratelimit.NewBucketWithQuantum(time.Second, burst, packetsPerSecond))
+
+	if bkt, ok := cache.Get(key); ok {
+		return bkt.(*ratelimit.Bucket)
+	}
+
+	// if for some reasons the bucket gets evicted between adding and reading
+	// just try again ...
+	return getRateLimit(key)
+}
+
+// handle a network packet
+func handlePacket(p gopacket.Packet) {
+	flow := p.NetworkLayer().NetworkFlow()
+
+	// get the destination ip address
+	dst := flow.Dst().String()
+
+	// check if we need to count packets
+	if !locals[dst] && !offenders[dst] {
+		//log.Printf("%s -> %s", flow.Src(), flow.Dst())
+		bkt := getRateLimit(dst)
+		tokens := bkt.TakeAvailable(1)
+
+		// if all tokens are used, ban the ip address
+		// because it's most likely trying to abuse our bandwidth
+		if tokens == 0 {
+			offenders[dst] = true
+			log.Printf("Offender %s", dst)
+			output, err := exec.Command("iptables", "-A", "INPUT", "-s", dst, "-j", "DROP").CombinedOutput()
+			if err != nil {
+				log.Printf("Failed banning IP: %s %s %s", dst, err.Error(), string(output))
+			}
+		}
+	}
+}
+
+// build the filter string for pcap
+// it only applies to outgoing packets, stateless ET packets
+// this dramatically reduces the number of packets that need
+// to be lifted into user space for us
+func getFilterString() string {
+	if len(locals) == 0 {
+		return ""
+	}
+
+	addrs := make([]string, 0, len(locals))
+
+	for ip := range locals {
+		addrs = append(addrs, ip)
+	}
+
+	return fmt.Sprintf("(src host %s) and udp and udp[8:4] = 0xFFFFFFFF", strings.Join(addrs, " or "))
+}
+
+func main() {
+	getLocalAddresses()
+	log.Print(getFilterString())
+
+	//if handle, err := pcap.OpenOffline("attack3.pcap"); err != nil {
+	if handle, err := pcap.OpenLive("any", 512, false, pcap.BlockForever); err != nil {
+		panic(err)
+	} else if err := handle.SetBPFFilter(getFilterString()); err != nil {
+		panic(err)
+	} else {
+		//var offset time.Duration
+
+		packetSource := gopacket.NewPacketSource(handle, handle.LinkType())
+		for packet := range packetSource.Packets() {
+
+			/*
+				// delay to simulate actual time span
+				if offset == 0 {
+					offset = time.Now().Sub(packet.Metadata().Timestamp)
+				}
+
+				sleep := packet.Metadata().Timestamp.Add(offset).Sub(time.Now())
+				time.Sleep(sleep)
+			*/
+
+			handlePacket(packet)
+		}
+	}
+}
